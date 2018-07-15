db8c2e5bdac2d28c45d3bc10efde16991c188794
Dark-Man1234 <38016447+Dark-Man1234@users.noreply.github.com>
2018-07-15 19:47:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 var prefix = "/";
 
-client.login(prossec.env.TOKEN)
+client.login(process.env.TOKEN)
 
 client.on("ready", () => {
     console.log("A votre service My Lord");
